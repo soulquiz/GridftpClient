b8bdd20a50de7aacfe1410bdebe1b24d4fd6e2b9
Chatchai Honarongsiri <soulquiza@gmail.com>
2018-05-12 18:14:33 +0700

    change variable style from underscore to camel style

--- index.js
+++ index.js
@@ -124,11 +124,11 @@ function transferFile (req, res, sourceName, desName, method) {
               var filePath = '/home/guser/Desktop/'
               var sourceFile = JSON.parse(req.body.sourceFile)
 
-              sourceFile.file_size = sourceFile.file_size.split(' ').join('') // remove space for filesizeParser
+              sourceFile.fileSize = sourceFile.fileSize.split(' ').join('') // remove space for filesizeParser
 
-              var file_size = filesizeParser(sourceFile.file_size, { base: 10 }) / 1000000 // file size in number MB
-              console.log('filesize: ' + file_size + ' MB')
-              var command = "globus-url-copy -cd -r 'gsiftp://" + sourceName + filePath + sourceFile.file_name + "' 'gsiftp://" + desName + filePath + sourceFile.file_name + "'"
+              var fileSize = filesizeParser(sourceFile.fileSize, { base: 10 }) / 1000000 // file size in number MB
+              console.log('filesize: ' + fileSize + ' MB')
+              var command = "globus-url-copy -cd -r 'gsiftp://" + sourceName + filePath + sourceFile.fileName + "' 'gsiftp://" + desName + filePath + sourceFile.fileName + "'"
               var start = now() // keep start time for measure transfer performance
               cmd.get('./myproxy-login.sh \n ' + command,
                 function (err, data, stderr) {
@@ -138,10 +138,10 @@ function transferFile (req, res, sourceName, desName, method) {
                     var duration = ((end - start) / 1000).toFixed(2) // change millisec to sec
                     console.log('duration: ' + duration + ' seconds')
 
-                    var speed = (file_size / duration).toFixed(2) // speed with 2 decimals
+                    var speed = (fileSize / duration).toFixed(2) // speed with 2 decimals
                     console.log('speed: ' + speed + ' MB/s')
 
-                    var status = method + ' ' + sourceFile.file_name + ' success in ' + duration + ' seconds ' + '(' + speed + ' MB/s)'
+                    var status = method + ' ' + sourceFile.fileName + ' success in ' + duration + ' seconds ' + '(' + speed + ' MB/s)'
                     gridftpRedirect(req, res, status)
                   } else {
                     console.log(err)
@@ -163,7 +163,7 @@ app.post('/local_delete', function (req, res) {
   console.log(req.body)
   var filePath = '/home/guser/Desktop/'
   var sourceFile = JSON.parse(req.body.sourceFile)
-  var filename = sourceFile.file_name
+  var filename = sourceFile.fileName
   deleteFile(filePath, filename, function (err) {
     if (!err) { // check if not error while delete file
       var status = 'Delete ' + filename + ' success'
@@ -183,18 +183,18 @@ app.post('/remote_delete', function (req, res) {
   request.post({
     headers: { 'content-type': 'application/x-www-form-urlencoded' },
     url: 'http://' + req.body.desIp + ':8080' + '/delete',
-    body: 'sourceFile=' + sourceFile.file_name
+    body: 'sourceFile=' + sourceFile.fileName
   }, function (error, response, body) {
     if (!error) { // check if not error while request
-      var body_obj = JSON.parse(body)
+      var bodyObj = JSON.parse(body)
 
-      if (!body_obj.err) { // check if not error while delete file
-        var status = 'Delete ' + sourceFile.file_name + ' success'
+      if (!bodyObj.err) { // check if not error while delete file
+        var status = 'Delete ' + sourceFile.fileName + ' success'
         gridftpRedirect(req, res, status)
       } else { // if error while delete file
-        console.log(body_obj.err)
+        console.log(bodyObj.err)
 
-        status = 'Error while delete file ' + sourceFile.file_name
+        status = 'Error while delete file ' + sourceFile.fileName
         gridftpRedirect(req, res, status)
       }
     } else { // if error while request to delete
@@ -242,20 +242,20 @@ app.get('/gridftp', function (req, res) {
       cmd.get('hostname', function (err, data, stderr) { // get source hostname
         if (!err) {
           data = data.replace('\n', '') // remove \n from data return
-          var context = { source_hostname: data }
+          var context = { sourceHostName: data }
 
           cmd.get('hostname -I', function (err, data, stderr) { // get souece ip
             if (!err) {
               context.sourceIp = data.replace('\n', '') // remove \n from data return
               getFileList('localhost', 8080, function (body) {
-                context.file_list = body // get file list
+                context.fileList = body // get file list
 
                 context.desName = req.query.desName // get destination information from form
                 context.desIp = req.query.desIp
                 context.status = req.query.status
 
                 getFileList(context.desIp, 8080, function (result) {
-                  context.des_file_list = result // get file list of destination host
+                  context.desFileList = result // get file list of destination host
                   res.render('gridftp', context)
                 })
               })
@@ -280,23 +280,23 @@ app.get('/listfile', function (req, res) {
   cmd.get(
     'ls -shp --block-size=MB ' + filePath, function (err, data, stderr) {
       if (!err) {
-        var file_size_arr = data.split('\n')
-        file_size_arr.splice(-1, 1) // remove " " last element
-        file_size_arr.splice(0, 1) // remove " total information " at first element
+        var fileSizeArr = data.split('\n')
+        fileSizeArr.splice(-1, 1) // remove " " last element
+        fileSizeArr.splice(0, 1) // remove " total information " at first element
 
         // get only file name
         cmd.get('ls -p ' + filePath, function (err, data, stderr) {
           if (!err) {
-            var file_name_arr = data.split('\n')
-            file_name_arr.splice(-1, 1) // remove " " last element
-            var file_size_obj = {}
-            for (var i = 0; i < file_size_arr.length; i++) {
-              if (!file_name_arr[i].includes('\\')) { // ignore file that has \ in filename because gridftp not support
-                file_size_obj[file_name_arr[i]] = file_size_arr[i].replace(file_name_arr[i], '') // remove file name from string file size+filename
+            var fileNameArr = data.split('\n')
+            fileNameArr.splice(-1, 1) // remove " " last element
+            var fileSizeObj = {}
+            for (var i = 0; i < fileSizeArr.length; i++) {
+              if (!fileNameArr[i].includes('\\')) { // ignore file that has \ in filename because gridftp not support
+                fileSizeObj[fileNameArr[i]] = fileSizeArr[i].replace(fileNameArr[i], '') // remove file name from string file size+filename
               }
             }
 
-            res.json(file_size_obj)
+            res.json(fileSizeObj)
           } else {
             console.log('error', err)
           }
