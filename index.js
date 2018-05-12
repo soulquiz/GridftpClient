79fd938ba6ce78eef941f2658dac94c430331329
Chatchai Honarongsiri <soulquiza@gmail.com>
2018-05-12 16:56:30 +0700

    add eslint for check coding standard style

--- index.js
+++ index.js
@@ -1,52 +1,47 @@
-var alert = require('alert-node');
-var express = require('express');
-var path = require('path');
-var cmd = require('node-cmd');
-var app = express();
-var bodyParser = require('body-parser');
-var request = require('request');
-var fs = require('fs');
-var url = require('url');
-var now = require("performance-now")
-var filesizeParser = require('filesize-parser');
+var express = require('express')
+var path = require('path')
+var cmd = require('node-cmd')
+var app = express()
+var bodyParser = require('body-parser')
+var request = require('request')
+var url = require('url')
+var now = require('performance-now')
+var filesizeParser = require('filesize-parser')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 // Setup template engines go here.
-
-
 // home page
 app.get('/', function (req, res) {
   var context = req.query
   res.render('index', context)
 })
 
-function checkConnectivity(des_hostname, des_port, next) {
+function checkConnectivity (des_hostname, des_port, next) {
   cmd.get('ping -c 1 ' + des_hostname, function (err, data, stderr) {
     if (!err) {
-      var des_ip = data.split(" ")[2].replace('(', '').replace(')', '');  // remove () from data return
+      var des_ip = data.split(' ')[2].replace('(', '').replace(')', '')  // remove () from data return
 
       cmd.get('nmap -p ' + des_port + ' ' + des_hostname, function (err, data, stderr) {
         var result = data.split(" ")
         if (result.indexOf("open") > -1) {  // if port open 
           console.log(des_hostname + ':' + des_port + '  ' + 'open')
-          var status = des_hostname + ':' + des_port + '  ' + 'open';
+          var status = des_hostname + ':' + des_port + '  ' + 'open'
           return next(true, des_ip, null, status)  // connectivity, des_ip, error, status
         } else {  // if port closed
           console.log(des_hostname + ':' + des_port + '  ' + 'closed')
-          var status = des_hostname + ':' + des_port + '  ' + 'closed';
+          var status = des_hostname + ':' + des_port + '  ' + 'closed'
           return next(false, null, err, status)  // connectivity, des_ip, error, status
         }
       })
     } else {
       console.log(des_hostname + '  ' + 'closed device')
-      var status = des_hostname + '  ' + 'closed device';
+      var status = des_hostname + '  ' + 'closed device'
       return next(false, null, err, status)  // connectivity, des_ip, error
     }
   })
@@ -65,7 +60,7 @@ app.get('/check_connect', function (req, res) {
         pathname: "/",
         query: context
       })
-      );
+      )
     } else {  // can connect
       var context = req.query
       context.des_ip = des_ip
@@ -74,16 +69,16 @@ app.get('/check_connect', function (req, res) {
         pathname: "/gridftp",
         query: context
       })
-      );
+      )
     }
   })
-});
+})
 
 // internal function
 function getFileList(ip, port, next) {
   request('http://' + ip + ':' + port + '/listfile', function (error, response, body) {
     next(JSON.parse(body))
-  });
+  })
 }
 
 function deleteFile(file_path, filename, next) {
@@ -105,7 +100,7 @@ function gridftpRedirect(req, res, status) {
     pathname: "/gridftp",
     query: context
   })
-  );
+  )
 }
 
 // transfer via gsiftp 
@@ -153,14 +148,14 @@ function transferFile(req, res, source_name, des_name, method) {
                     var speed = (file_size / duration).toFixed(2)  // speed with 2 decimals
                     console.log('speed: ' + speed + ' MB/s')
 
-                    var status = method + ' ' + source_file.file_name + ' success in ' + duration + ' seconds ' + '(' + speed + ' MB/s)';
+                    var status = method + ' ' + source_file.file_name + ' success in ' + duration + ' seconds ' + '(' + speed + ' MB/s)'
                     gridftpRedirect(req, res, status)
 
 
                   } else {
                     console.log(err)
 
-                    var status = 'Error in grid authentication or transfering or permission denied file';
+                    var status = 'Error in grid authentication or transfering or permission denied file'
                     gridftpRedirect(req, res, status)
 
                   }
@@ -181,12 +176,12 @@ app.post('/local_delete', function (req, res) {
   var filename = source_file.file_name
   deleteFile(file_path, filename, function (err) {
     if (!err) {  // check if not error while delete file
-      var status = 'Delete ' + filename + ' success';
+      var status = 'Delete ' + filename + ' success'
       gridftpRedirect(req, res, status)
 
     } else {  // check if error while delete file
       console.log(err)
-      var status = 'Error while delete file ' + filename;
+      var status = 'Error while delete file ' + filename
       gridftpRedirect(req, res, status)
     }
   })
@@ -205,13 +200,13 @@ app.post('/remote_delete', function (req, res) {
       var body_obj = JSON.parse(body)
 
       if (!body_obj.err) {  // check if not error while delete file
-        var status = 'Delete ' + source_file.file_name + ' success';
+        var status = 'Delete ' + source_file.file_name + ' success'
         gridftpRedirect(req, res, status)
 
       } else {  // if error while delete file
         console.log(body_obj.err)
 
-        var status = 'Error while delete file ' + source_file.file_name;
+        var status = 'Error while delete file ' + source_file.file_name
         gridftpRedirect(req, res, status)
 
       }
@@ -219,11 +214,11 @@ app.post('/remote_delete', function (req, res) {
     } else {  // if error while request to delete
       console.log(error)
 
-      var status = 'Error while delete request to remote site ';
+      var status = 'Error while delete request to remote site '
       gridftpRedirect(req, res, status)
 
     }
-  });
+  })
 })
 
 // delete file in Desktop directory and return status json
@@ -238,13 +233,13 @@ app.post('/delete', function (req, res) {
 
 // transfer file upload by gsiftp
 app.post('/upload', function (req, res) {
-  transferFile(req, res, req.body.source_name, req.body.des_name, 'Upload');
+  transferFile(req, res, req.body.source_name, req.body.des_name, 'Upload')
 })
 
 
 // transfer file download by gsiftp
 app.post('/download', function (req, res) {
-  transferFile(req, res, req.body.des_name, req.body.source_name, 'Download');
+  transferFile(req, res, req.body.des_name, req.body.source_name, 'Download')
 })
 
 // gridftp page
@@ -259,7 +254,7 @@ app.get('/gridftp', function (req, res) {
         pathname: "/",
         query: context
       })
-      );
+      )
     } else {  // can connect
       cmd.get('hostname', function (err, data, stderr) {  // get source hostname
         if (!err) {
@@ -312,7 +307,7 @@ app.get('/listfile', function (req, res) {
     'ls -shp --block-size=MB ' + file_path, function (err, data, stderr) {
       if (!err) {
 
-        var file_size_arr = data.split("\n");
+        var file_size_arr = data.split("\n")
         file_size_arr.splice(-1, 1)  // remove " " last element
         file_size_arr.splice(0, 1)  // remove " total information " at first element
 
@@ -320,9 +315,9 @@ app.get('/listfile', function (req, res) {
         // get only file name 
         cmd.get('ls -p ' + file_path, function (err, data, stderr) {
           if (!err) {
-            var file_name_arr = data.split("\n");
+            var file_name_arr = data.split("\n")
             file_name_arr.splice(-1, 1)  // remove " " last element
-            var file_size_obj = {};
+            var file_size_obj = {}
             for (var i = 0; i < file_size_arr.length; i++) {
 
               if (!file_name_arr[i].includes('\\')) {  // ignore file that has \ in filename because gridftp not support
@@ -348,6 +343,6 @@ var server = app.listen(8080, function () {
 
   var host = 'localhost'
   var port = server.address().port
-  console.log("GridFTP App listening at http://%s:%s", host, port);
+  console.log("GridFTP App listening at http://%s:%s", host, port)
 
-});
+})
